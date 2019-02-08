e1749628c82edc44f9c3728c3a5fb4aa0cd9e24d
jochemlam <30724588+jochemlam@users.noreply.github.com>
2019-02-08 10:48:50 +0100

    push stuff

---
+++ OpdrachtJS/Assets/Scripts/ControlFire.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlFire : MonoBehaviour {
+
+    public Action<string> FireBullet;
+
+    private void Awake()
+    {
+        FireBullet("Shots have been fired!");
+    }
+
+
+}
---
+++ OpdrachtJS/Assets/Scripts/Fire.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+
+    private void Awake()
+    {
+        ControlFire fire = GetComponent<ControlFire>();
+        fire.FireBullet += Shoot;
+    }
+
+    private void Shoot(string msg)
+    {
+        Debug.Log(msg);
+    }
+}
